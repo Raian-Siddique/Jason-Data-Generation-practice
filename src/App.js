e8081832562e930ff0c84e3586227dbee4912d10
Raian-Siddique <raian0197@gmail.com>
2021-01-07 06:02:05 +0600

    stwp 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import data from "./data/data.json";
+import { useEffect, useState } from "react";
+import { fakeData } from "./data/fakeData";
 
 function App() {
+  console.log(fakeData);
+  const [clubs, setClubs] = useState([]);
+
+  useEffect(() => {
+    setClubs(fakeData);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {clubs.map((club) => (
+          <li>{club.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
